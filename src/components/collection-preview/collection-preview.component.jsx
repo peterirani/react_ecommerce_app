e71234dfee892ac412783893c4f1824a78b4d7be
Peshotan Irani <peshotanirani@Peshotans-Mac-mini.local>
2020-05-15 23:54:15 -0700

    update(collection-preview-component) : revised the component to only show 4 items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import "./collection-preview.scss";
 
-export default function CollectionPreview({title, collections}) {
+export default function CollectionPreview({title, items}) {
     return(
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {collections.map(item => (<div key={item.id}>{item.name}</div>))}
+                {items
+                    .filter((item,idx) => (idx < 4))
+                    .map(item => (
+                        <div key={item.id}>{item.name}</div>
+                    ))}
             </div>
         </div>
     )
