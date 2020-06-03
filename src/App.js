5f8a8645a78d620cb1b53c13dac23507f27e580c
peterirani <peshotanirani@gmail.com>
2020-06-02 21:37:34 -0700

    format change

--- src/App.js
+++ src/App.js
@@ -11,12 +11,6 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import { auth } from "./firebase/firebase.utils";
 
 
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 9c0d5118f3d5f1662255a63f0331aa5334d31047
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -25,41 +19,34 @@ class App extends React.Component {
         }
     }
 
-<<<<<<< HEAD
     componentDidMount() {
         auth.onAuthStateChanged(user => {
-=======
-    // to unsubscribe from auth connection
-    unsubscribeFromAuth = null;
+            // to unsubscribe from auth connection
+            let unsubscribeFromAuth = null;
+        });
+    }
 
-    componentDidMount() {
+    componentDidMount(){
         this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
->>>>>>> 9c0d5118f3d5f1662255a63f0331aa5334d31047
             this.setState({currentUser : user})
         })
     }
 
-<<<<<<< HEAD
 
-    render(){
-=======
-    componentWillUnmount() {
+
+    componentWillUnmount(){
         this.unsubscribeFromAuth();
     }
 
 
     render() {
->>>>>>> 9c0d5118f3d5f1662255a63f0331aa5334d31047
         return (
             <div className="App">
                 <header className="App-header">
                     <Header />
                     <Switch>
                         <Route  path='/shop' component={ShopPage} />
-<<<<<<< HEAD
                         {/*<Route  path='/hats' component={HatsPage} />*/}
-=======
->>>>>>> 9c0d5118f3d5f1662255a63f0331aa5334d31047
                         <Route  path='/collections' component={CollectionPreview} />
                         <Route  path='/signin' component={SignInAndSignUpPage} />
                         <Route  path='/' component={Homepage} />
@@ -68,10 +55,6 @@ class App extends React.Component {
             </div>
         );
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 9c0d5118f3d5f1662255a63f0331aa5334d31047
 }
 
 export default App;
