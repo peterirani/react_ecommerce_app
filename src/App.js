52fe4b93e41fa8ae1dad2d274719248bf7a5514d
peterirani <peshotanirani@gmail.com>
2020-06-02 21:31:22 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import  { Route, Switch } from "react-router-dom";
+import { auth } from "firebase/firebase.utils"
 
 import './App.css';
 import Homepage from "./pages/homepage.component";
@@ -9,28 +10,41 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Header />
-          <Switch>
-              <Route  path='/shop' component={ShopPage} />
-              <Route  path='/hats' component={HatsPage} />
-              <Route  path='/collections' component={CollectionPreview} />
-              <Route  path='/signin' component={SignInAndSignUpPage} />
-              <Route  path='/' component={Homepage} />
-          </Switch>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser : null
+        }
+    }
+
+    componentDidMount() {
+        auth.onAuthStateChanged(user => {
+            this.setState({currentUser : user})
+        })
+    }
+
+
+    render(){
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <Header />
+                    <Switch>
+                        <Route  path='/shop' component={ShopPage} />
+                        {/*<Route  path='/hats' component={HatsPage} />*/}
+                        <Route  path='/collections' component={CollectionPreview} />
+                        <Route  path='/signin' component={SignInAndSignUpPage} />
+                        <Route  path='/' component={Homepage} />
+                    </Switch>
+                </header>
+            </div>
+        );
+    }
+
 }
 
 export default App;
